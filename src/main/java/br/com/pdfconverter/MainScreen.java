c23c4966cf48c279f79943b26246c73ac87edad8
William Lima <williamlims@hotmail.com>
2022-08-15 22:05:47 -0300

    update

--- src/main/java/br/com/pdfconverter/MainScreen.java
+++ src/main/java/br/com/pdfconverter/MainScreen.java
@@ -4,7 +4,18 @@
  */
 package br.com.pdfconverter;
 
+import java.awt.Component;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import org.apache.poi.xwpf.converter.pdf.PdfConverter;
+import org.apache.poi.xwpf.converter.pdf.PdfOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 /**
  *
@@ -99,10 +110,28 @@ public class MainScreen extends javax.swing.JFrame {
         int testVal = file.showOpenDialog(file);
         if(testVal ==  JFileChooser.APPROVE_OPTION){
             txtNomeArquivo.setText(file.getSelectedFile().getAbsolutePath());
-            //file.getSelectedFile().getAbsoluteFile()
+            String nome = file.getSelectedFile().getName();
+            nome = nome.substring(0, nome.length() - 4);
+            ConvertToPDF(file.getSelectedFile().getAbsolutePath(), "C:/"+nome+".pdf");
+            
+            Component frame = null;
+            JOptionPane.showMessageDialog(frame, "O PDF foi gerado em C:/ ");
         }
     }//GEN-LAST:event_btnAbrirArquivoActionPerformed
-
+    
+    public void ConvertToPDF(String docPath, String pdfPath){
+        try {
+            InputStream doc = new FileInputStream(new File(docPath));
+            XWPFDocument document = new XWPFDocument(doc);
+            PdfOptions options = PdfOptions.create();
+            OutputStream out = new FileOutputStream(new File(pdfPath));
+            PdfConverter.getInstance().convert(document, out, options);
+        } catch (IOException ex) {
+            Component frame = null;
+            JOptionPane.showMessageDialog(frame, "Ouve um erro ao executar a ação: " +ex);
+        }
+    }
+    
     private void btnLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimparActionPerformed
         txtNomeArquivo.setText("");
     }//GEN-LAST:event_btnLimparActionPerformed
